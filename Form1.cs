4819f460cf7e0120515b452c2f4524ef5c1cf24e
Meawz <maftei.cristian23@gmail.com>
2023-09-05 12:51:26 +0300

    Added an XML file as a login database and corrected Form1 code

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Linq;
 
 namespace WindowsFormsAppTest
 {
     public partial class Form1 : Form
     {
 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string FromXML_user = "";
+        public string FromXML_pass = "";
+        public string FromXML_name = "";
+
         private void ClearTextBoxes()
         {
             textBoxUser.Clear();
             textBoxPass.Clear();
-        }
-
-        public Form1()
-        {
-            InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -29,6 +37,25 @@ namespace WindowsFormsAppTest
 
         private void ButtonLogin_Click(object sender, EventArgs e)
         {
+
+            XDocument doc = XDocument.Load(Application.StartupPath.ToString() + @"\LoginInfo.xml");
+            var selected_user = from x in doc.Descendants("users").Where
+                                (x => (string)x.Element("username") == textBoxUser.Text)
+                                select new
+                                {
+                                    XMLuser = x.Element("username").Value,
+                                    XMLpass = x.Element("password").Value,
+                                    XMLname = x.Element("name").Value
+                                };
+
+                        foreach( var x in selected_user)
+                         {
+                            FromXML_user = x.XMLuser;
+                            FromXML_pass = x.XMLpass;
+                            FromXML_name = x.XMLname;
+                         }
+
+
             if (textBoxUser.Text == " ")
             {
                 MessageBox.Show("Enter User!");
@@ -38,17 +65,17 @@ namespace WindowsFormsAppTest
                 MessageBox.Show("Enter Password!");
             }
             
-            else if (textBoxUser.Text != "Test")
+            else if (textBoxUser.Text != FromXML_user)
             {
                 MessageBox.Show("Invalid username!");
                 ClearTextBoxes();
             }
-            else if (textBoxUser.Text != "123")
+            else if (textBoxPass.Text != FromXML_pass)
             {
                 MessageBox.Show("Invalid password");
                 ClearTextBoxes();
             }
-            else if (textBoxUser.Text == "Test" && textBoxPass.Text == "123")
+            else if (textBoxUser.Text == FromXML_user && textBoxPass.Text == FromXML_pass)
             {
                 this.Hide();
                 Form2 Menu = new Form2();
